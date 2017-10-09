545f194bf97b954a63337621420dca78d49fe64e
ngengchhengkim <chhengkimngeng@gmail.com>
2017-10-09 14:51:36 +0700

    recruiter complete registration

--- src/component/index.js
+++ src/component/index.js
@@ -6,3 +6,4 @@ export * from "./TextField";
 export * from "./Title";
 export * from "./SegmentControl";
 export * from "./Label";
+export * from "./InputSelect";
--- src/screen/session/index.js
+++ src/screen/session/index.js
@@ -13,8 +13,38 @@ import {
   FacebookButton,
   TextField,
   Container,
-  Label
+  Label, InputSelect
 } from "./../../component";
+
+const items = [{
+    id: '92iijs7yta',
+    name: 'Ondo',
+  }, {
+    id: 'a0s0a8ssbsd',
+    name: 'Ogun',
+  }, {
+    id: '16hbajsabsd',
+    name: 'Calabar',
+  }, {
+    id: 'nahs75a5sg',
+    name: 'Lagos',
+  }, {
+    id: '667atsas',
+    name: 'Maiduguri',
+  }, {
+    id: 'hsyasajs',
+    name: 'Anambra',
+  }, {
+    id: 'djsjudksjd',
+    name: 'Benue',
+  }, {
+    id: 'sdhyaysdj',
+    name: 'Kaduna',
+  }, {
+    id: 'suudydjsjd',
+    name: 'Abuja',
+  }];
+
 
 class Session extends Component {
   render () {
